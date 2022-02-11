868cdf156532f54a16c4690ca7cb181c7c3b5d1f
dgs97 <vlad.senko1@mail.ru>
2022-02-11 15:57:55 +0300

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+from distutils.command.upload import upload
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to = "event_image/")
